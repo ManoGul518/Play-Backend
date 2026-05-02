b534111ee9b1ae32e394a3cce6e4f95d097b9e5e
ManoGul <mayagul0331@gmail.com>
2026-05-02 14:41:50 +0500

    Connected to Database

---
+++ src/index.js
@@ -0,0 +1,25 @@
+// Alternate method
+// require('dotenv').config({path: "./env"});
+import dotenv from "dotenv";
+import connectDB from "./db/index.js"
+dotenv.config({path: "./env "});
+
+connectDB();
+
+// Alternative approach - using IIFE 
+// (async () => {
+//   try {
+//     await mongoose.connect(`${process.env.MONGO_URI}/${DB_NAME}`);
+//     app.on("error", (err) => {
+//       console.log("ERR: ", err);
+//       throw err;
+//     });
+
+//     app.listen(process.env.PORT, () => {
+//       console.log("Server is running on port ", process.env.PORT);
+//     });
+//   } catch (error) {
+//     console.error("Error connecting to MongoDB:", error);
+//     throw error;
+//   }
+// })();
