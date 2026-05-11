71749e90bc453c4c05357f34802eff15eaa50c0a
ManoGul <mayagul0331@gmail.com>
2026-05-11 20:07:57 +0500

    Add user controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,9 +1,72 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
+import { ApiError } from "../utils/ApiError.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
+import { User } from "../models/user.model.js";
+import { uploadOnCloudinary } from "../utils/cloudinary.js";
 
 const registerUser = asyncHandler(async (req, res) => {
-    res.status(200).json({
-        message: "chai aur code",
+    // take the data from user
+    // validate it
+    // check if user already exists - username and email
+    // check for images and avatar
+    // upload the files on cloudinary
+    // create user object - entry in db
+    // remove password and refresh token field from response
+    // check if user is created
+    // return response
+
+    const { username, fullName, email, password } = req.body;
+
+    if (
+        [fullName, email, username, password].some(
+            (field) => field?.trim() === ""
+        )
+    ) {
+        throw new ApiError(400, "All fields are required");
+    }
+
+    const existedUser = User.findOne({
+        $or: [{ username }, { email }],
     });
+
+    if (existedUser) {
+        throw new ApiError(409, "User with email or username already exists");
+    }
+
+    const avatarLocalPath = req.files?.avatar[0].path;
+    const coverImageLocalPath = req.files?.coverImage[0]?.path;
+
+    if (!avatarLocalPath) {
+        throw new ApiError(400, "Avatar image is required");
+    }
+
+    const avatar = await uploadOnCloudinary(avatarLocalPath);
+    const coverImage = await uploadOnCloudinary(coverImageLocalPath);
+
+    if (!avatar) {
+        throw new ApiError(400, "Avatar file is required");
+    }
+
+    const user = await User.create({
+        fullName,
+        avatar: avatar.url,
+        coverImage: coverImage?.url || "",
+        email,
+        password,
+        username: username.toLowerCase(),
+    });
+
+    const createdUser = await User.findById(user._id).select(
+        "-password -refreshToken"
+    )
+
+    if (!createdUser) {
+        throw new ApiError(500, "Something went wrong while registering the user")
+    }
+
+    return res.status(201).json(
+        new ApiResponse(200, createdUser, "User registered successfully")
+    )
 });
 
 export { registerUser };
--- src/index.js
+++ src/index.js
@@ -5,13 +5,13 @@ import connectDB from "./db/index.js";
 dotenv.config({ path: "./env " });
 import { app } from "./app.js";
 
-connectDB()
-    .then(() => {
-        app.listen(process.env.PORT || 8000, () => {
-            console.log("Server is running on port", process.env.PORT);
-        });
-    })
-    .catch((err) => console.error("Error connecting to MongoDB:", err));
+// connectDB()
+//     .then(() => {
+//         app.listen(process.env.PORT || 8000, () => {
+//             console.log("Server is running on port", process.env.PORT);
+//         });
+//     })
+//     .catch((err) => console.error("Error connecting to MongoDB:", err));
 
 // Alternative approach - using IIFE
 // (async () => {
