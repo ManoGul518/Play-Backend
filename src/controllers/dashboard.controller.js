7af4b2308c7a590d8572d4852093ababe3f0e5bc
Mano <noormedia871@gmail.com>
2026-06-09 18:55:33 +0500

    Implement channel statistics retrieval and video fetching functionality

--- src/controllers/dashboard.controller.js
+++ src/controllers/dashboard.controller.js
@@ -2,16 +2,77 @@ import mongoose from "mongoose";
 import { Video } from "../models/video.model.js";
 import { Subscription } from "../models/subscription.model.js";
 import { Like } from "../models/like.model.js";
-import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 
 const getChannelStats = asyncHandler(async (req, res) => {
-    // TODO: Get the channel stats like total video views, total subscribers, total videos, total likes etc.
+    const [totalVideos, totalSubscribers, totalLikesResult, totalViewsResult] =
+        await Promise.all([
+            Video.countDocuments({ owner: req.user._id }),
+            Subscription.countDocuments({ channel: req.user._id }),
+            Like.aggregate([
+                {
+                    $lookup: {
+                        from: "videos",
+                        localField: "video",
+                        foreignField: "_id",
+                        as: "videoDetails",
+                    },
+                },
+                {
+                    $unwind: "$videoDetails",
+                },
+                {
+                    $match: {
+                        "videoDetails.owner": new mongoose.Types.ObjectId(
+                            req.user._id
+                        ),
+                    },
+                },
+                {
+                    $group: {
+                        _id: null,
+                        totalLikes: { $sum: 1 },
+                    },
+                },
+            ]),
+            Video.aggregate([
+                {
+                    $match: {
+                        owner: new mongoose.Types.ObjectId(req.user._id),
+                    },
+                },
+                {
+                    $group: {
+                        _id: null,
+                        totalViews: { $sum: "$views" },
+                    },
+                },
+            ]),
+        ]);
+
+    return res.status(200).json(
+        new ApiResponse(
+            200,
+            {
+                totalVideos,
+                totalSubscribers,
+                totalLikes: totalLikesResult[0]?.totalLikes || 0,
+                totalViews: totalViewsResult[0]?.totalViews || 0,
+            },
+            "Channel stats fetched successfully"
+        )
+    );
 });
 
 const getChannelVideos = asyncHandler(async (req, res) => {
-    // TODO: Get all the videos uploaded by the channel
+    const videos = await Video.find({ owner: req.user._id }).sort({
+        createdAt: -1,
+    });
+
+    return res
+        .status(200)
+        .json(new ApiResponse(200, videos, "Videos fetched successfully"));
 });
 
 export { getChannelStats, getChannelVideos };
